a1e7d1d407f3d684426691b492818aa0b9530a06
leo <huangliquan@xzrobot.com>
2024-03-03 14:10:57 +0000

    更新堆栈捕获

--- main.cpp
+++ main.cpp
@@ -23,8 +23,66 @@
 #include "rk_mpp/rk_mpp_encoder.h"
 #endif
 
+#ifdef BACKTRACE_DEBUG
+#include <signal.h>
+#include <execinfo.h>
+
+#define PRINT_SIZE_ 100
+
+static void _signal_handler(int signum)
+{
+    void *array[PRINT_SIZE_];
+    char **strings;
+
+    size_t size = backtrace(array, PRINT_SIZE_);
+    strings = backtrace_symbols(array, size);
+
+    if (strings == nullptr) {
+	   fprintf(stderr, "backtrace_symbols");
+	   exit(EXIT_FAILURE);
+    }
+
+    switch(signum) {
+        case SIGSEGV:
+        fprintf(stderr, "widebright received SIGSEGV! Stack trace:\n");
+        break;
+
+        case SIGPIPE:
+        fprintf(stderr, "widebright received SIGPIPE! Stack trace:\n");
+        break;
+
+        case SIGFPE:
+        fprintf(stderr, "widebright received SIGFPE! Stack trace:\n");
+        break;
+
+        case SIGABRT:
+        fprintf(stderr, "widebright received SIGABRT! Stack trace:\n");
+        break;
+
+        default:
+        break;
+    }
+
+    for (size_t i = 0; i < size; i++) {
+        fprintf(stderr, "%zu %s \n", i, strings[i]);
+    }
+
+    free(strings);
+    signal(signum, SIG_DFL); /* 还原默认的信号处理handler */
+
+    exit(1);
+}
+#endif
+
 int main(int argc, char **argv)
 {
+#ifdef BACKTRACE_DEBUG
+    signal(SIGPIPE, _signal_handler);  // SIGPIPE，管道破裂。
+    signal(SIGSEGV, _signal_handler);  // SIGSEGV，非法内存访问
+    signal(SIGFPE, _signal_handler);  // SIGFPE，数学相关的异常，如被0除，浮点溢出，等等
+    signal(SIGABRT, _signal_handler);  // SIGABRT，由调用abort函数产生，进程非正常退出
+#endif
+
     spdlog::info("chip hardware concurrency {} !", std::thread::hardware_concurrency());
 
 #if defined(USE_RK_HW_ENCODER)
